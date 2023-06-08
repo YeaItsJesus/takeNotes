72abb60e5019032afbf3cba0a693400add1731f4
YeaItsJesus <122085651+YeaItsJesus@users.noreply.github.com>
2023-06-08 16:15:05 -0500

    edited webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -55,6 +55,24 @@ module.exports = () => {
           test: /\.css$/i,
           use: ["style-loader", "css-loader"],
         },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          }
+        }
         
       ],
     },
