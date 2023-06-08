243ba1ffec26a6906687a30e2711ac8e5e2873fc
YeaItsJesus <122085651+YeaItsJesus@users.noreply.github.com>
2023-06-08 16:23:00 -0500

    database method

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,7 +13,18 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+const jateDB = await openDB("jate", 1);
+  //New trans
+  const tx = jateDB.transaction("jate", "readwrite");
+ // desired obj
+  const store = tx.objectStore("jate");
+  
+  const request = store.put({ id: 1, value: content });
+  
+  const result = await request;
+  console.log("aved to the database", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
